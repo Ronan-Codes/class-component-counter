6cd281eabe0ec03fa34d41a6b826cff430c4c93f
Ronan-Codes <galv.ronan@gmail.com>
2022-04-14 20:41:34 -0400

    finished hw

--- class-components/src/components/ClassCounter/ClassCounter.js
+++ class-components/src/components/ClassCounter/ClassCounter.js
@@ -1,39 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ClassCounter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { counter: 0 }
-    }
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
 
-    render() {
-        return (
-            <div className='counter'>
-                <h2>Class Component </h2>
-                <div className='counter__form'>
-                    <input type="text" value={this.state.counter} />
-                    <button 
-                        onClick={() => {
-                            this.setState({...this.state, counter: this.state.counter + 1 });
-                        }}>
-                        Add 1
-                    </button>
-                    <button 
-                        onClick={() => {
-                            this.setState({...this.state, counter: this.state.counter - 1 });
-                        }}>
-                        Minus 1
-                    </button>
-                    <button 
-                        onClick={() => {
-                            this.setState({...this.state, counter: this.state.counter = 0 });
-                        }}>
-                        Reset
-                    </button>
-                </div>
-            </div>
-        );
-    }
+  // It does not add 1 twice, because after setState-ing,
+  // it does it again, which cancels the first setState()
+  addOne = () => {
+    this.setState({ ...this.state, counter: this.state.counter + 1 });
+    this.setState({ ...this.state, counter: this.state.counter + 1 });
+  };
+
+  minusOne = () => {
+    this.setState({ ...this.state, counter: this.state.counter - 1 });
+  };
+
+  reset = () => {
+    this.setState({ ...this.state, counter: 0 });
+  };
+
+  alerter = () => {
+    //  Changed alert(counter) to alert(countRef.current)
+    setTimeout(() => alert(this.state.counter), 3000);
+  };
+
+  render() {
+    return (
+      <div className="counter">
+        <h2>Class Component </h2>
+        <div className="counter__form">
+          <input type="text" value={this.state.counter} />
+          <button onClick={this.addOne}>Add 1</button>
+          <button onClick={this.alerter}>Alerter</button>
+          <button onClick={this.minusOne}>Minus 1</button>
+          <button onClick={this.reset}>Reset</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ClassCounter;
--- class-components/src/components/FunctionalCounter/FunctionalCounter.js
+++ class-components/src/components/FunctionalCounter/FunctionalCounter.js
@@ -1,34 +1,45 @@
-import React, {useState} from 'react';
+import React, { useState, useRef } from "react";
 
 const FunctionalCounter = (props) => {
-    const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-    return (
-        <div className='counter'>
-                <h2>Functional Component  </h2>
-                <div className='counter__form'>
-                    <input type="text" value={counter} />
-                    <button
-                        onClick={() => {
-                            setCounter(counter + 1);
-                        }}>
-                        Add 1
-                    </button>
-                    <button 
-                        onClick={() => {
-                            setCounter(counter - 1);
-                        }}>
-                        Minus 1
-                    </button>
-                    <button 
-                        onClick={() => {
-                            setCounter(0);
-                        }}>
-                        Reset
-                    </button>
-                </div>
-        </div>
-    );
+  // HW fix with useRef
+  const countRef = useRef(counter);
+  countRef.current = counter;
+
+  const addOne = () => {
+    setCounter(counter + 1);
+  };
+
+  const alerter = () => {
+    //  Changed alert(counter) to alert(countRef.current)
+    setTimeout(() => alert(countRef.current), 3000);
+  };
+
+  return (
+    <div className="counter">
+      <h2>Functional Component </h2>
+      <div className="counter__form">
+        <input type="text" value={counter} />
+        <button onClick={addOne}>Add 1</button>
+        <button onClick={alerter}>Alert</button>
+        <button
+          onClick={() => {
+            setCounter(counter - 1);
+          }}
+        >
+          Minus 1
+        </button>
+        <button
+          onClick={() => {
+            setCounter(0);
+          }}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default FunctionalCounter;
